b2d60820bca24b28d63e57eb7dd11c59239af74d
Greattie <gamubode@gmail.com>
2024-05-23 14:12:26 +0100

    created navbar, hero and about section

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+export const About = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="my-20 text-center text--4xl">
+        About <span className="text-neutral">Me</span>
+      </h1>
+    </div>
+  );
+};
